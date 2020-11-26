c0172bd7ce4173391404d47713144676ba7b2ffc
Nathan Godey <nathan.godey@eleves.enpc.fr>
2020-11-26 18:06:08 +0100

    autodetect device

--- vocoder.py
+++ vocoder.py
@@ -3,9 +3,10 @@ import librosa
 import pickle
 from synthesis import build_model
 from synthesis import wavegen
+import soundfile as sf
+from torch_utils import device
 
 spect_vc = pickle.load(open('results.pkl', 'rb'))
-device = torch.device("cuda")
 model = build_model().to(device)
 checkpoint = torch.load("checkpoint_step001000000_ema.pth")
 model.load_state_dict(checkpoint["state_dict"])
@@ -15,4 +16,4 @@ for spect in spect_vc:
     c = spect[1]
     print(name)
     waveform = wavegen(model, c=c)
-    librosa.output.write_wav(name+'.wav', waveform, sr=16000)
+    sf.write(name+'.wav', waveform, 16000)
